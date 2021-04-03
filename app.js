35b6b81e5580096269388c99b49beece7416752b
abdouudrh <abdelrrahman3@gmail.com>
2021-04-03 12:43:54 +0100

    my first commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require ('express')
 const path = require("path")
+require('dotenv').config()
 
 const app = express()
 
-const port = 3001
+const port =  process.env.PORT 
 const publicDirectoryPath = path.join(__dirname, "./public")
 app.use(express.static(publicDirectoryPath))
 
